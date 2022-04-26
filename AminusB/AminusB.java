d9bf02ba142d6facaf3e72ad61835fb14912e3d0
MoonHo <75512752+monho@users.noreply.github.com>
2022-04-26 19:08:42 +0900

    A-B, A+B 문제 추가 (백준 1001번, 1000번 문제)

---
+++ AminusB/AminusB.java
@@ -0,0 +1,14 @@
+package AminusB;
+import java.util.Scanner;
+public class AminusB {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A-B);
+
+        in.close();
+    }
+}
---
+++ AplusB/AplusB.java
@@ -0,0 +1,16 @@
+package AplusB;
+
+import java.util.Scanner;
+public class AplusB {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A+B);
+
+        in.close();
+    }
+
+}
