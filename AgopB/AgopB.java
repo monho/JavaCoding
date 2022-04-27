039674d70e00dfcb58f8b57dc6de99743c931d3b
MoonHo <75512752+monho@users.noreply.github.com>
2022-04-27 17:36:40 +0900

    AnanogiB 문제 ( 백준 1008문제 ) 추가

--- AgopB/AgopB.java
+++ AgopB/AgopB.java
@@ -1,4 +1,18 @@
 package AgopB;
 
+import java.util.Scanner;
+
 public class AgopB {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A*B);
+
+        in.close();
+    }
+
 }
